896c48bf5796b4d7b8bdb554e9a67cec0e561c20
Md. Samiur Rahman Mukul <sr.mukul9090@gmail.com>
2021-04-04 10:12:58 +0600

    Footer Components Dynamic Data Generate

--- react-beach-resort-dev/src/Components/Footer/Footer.jsx
+++ react-beach-resort-dev/src/Components/Footer/Footer.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <h2>Beach Resort | Hotel Room Book</h2>
       <p>
-        &copy; 2021 BeachResort.com - All Rights Copyright Reserved -{" "}
-        <a href="https://www.facebook.com/SamiurRahmanMukul">
+        &copy; <span>{year}</span> BeachResort.com - All Rights Copyright
+        Reserved To{" "}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.facebook.com/SamiurRahmanMukul"
+        >
           Samiur Rahman Mukul
         </a>
       </p>
